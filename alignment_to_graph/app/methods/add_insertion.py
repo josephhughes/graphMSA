f997512a0201a9000e3d093665d96f9adbedfcbf
kcaj21 <alex.kcaj@gmail.com>
2025-06-13 13:34:03 +0100

    refactored add_insertion and add_deletion functions

--- alignment_to_graph/app/methods/add_insertion.py
+++ alignment_to_graph/app/methods/add_insertion.py
@@ -1,89 +1,66 @@
 def add_insertion(read, current_position, cigar_tuple):
     queries = []
     read_name = read.query_name
-    start = current_position
-    i = 0.01  # appending a decimal place to each insertion node, so they can be accounted for in numerical order before joining the final insertion base to the next reference sequence node
+    start_pos = current_position
+    insertion_length = cigar_tuple[1]
 
-    # create first insertion node
-    insertion_base = read.query_sequence[current_position - read.reference_start]
-    queries.append(
-        (
-            (
-                f"""
-            CREATE (n:Insertion {{pos: $insert_position, base: $base}})
-            WITH n
-            MATCH (n:Insertion {{pos: $insert_position}})
-            MATCH (l:Nucleotide {{pos: $pos}})
-            MERGE (l)-[:`{read_name}`]->(n)
-            """
-            ).strip(),
-            {
-                "pos": start,
-                "insert_position": current_position + i,
-                "base": insertion_base,
-            },
-        )
-    )
+    # Get the sequence of inserted bases from the read
+    read_offset = current_position - read.reference_start
+    inserted_bases = read.query_sequence[read_offset : read_offset + insertion_length]
 
-    if cigar_tuple[1] > 1:
-        i += 0.01
-        j = (
-            cigar_tuple[1] - 1 #decrement j by 1, before executing the while-loop, because we've added the first insertion node already
-        )
-        while j > 0:
+    prev_pos = None
+    for i, base in enumerate(inserted_bases):
+        insert_pos = start_pos + (i + 1) * 0.01
+
+        if i == 0:
+            # Link first insertion to reference position
             queries.append(
                 (
-                    (
-                        f"""
-                        CREATE (n:Insertion {{pos: $insert_position, base: $base}})
-                        WITH n
-                        MATCH (n:Insertion {{pos: $insert_position}})
-                        MATCH (l:Insertion {{pos: $last_position}})
-                        MERGE (l)-[:`{read_name}`]->(n)
-                        """
-                    ).strip(),
+                    f"""
+                CREATE (n:Insertion {{pos: $insert_pos, base: $base}})
+                WITH n
+                MATCH (l:Nucleotide {{pos: $ref_pos}})
+                MERGE (l)-[:`{read_name}`]->(n)
+                """.strip(),
                     {
-                        "insert_position": current_position + i,
-                        "last_position": current_position + (i - 0.01),
-                        "base": insertion_base,
+                        "insert_pos": insert_pos,
+                        "ref_pos": start_pos,
+                        "base": base,
+                    },
+                )
+            )
+        else:
+            # Link insertion to previous insertion
+            queries.append(
+                (
+                    f"""
+                CREATE (n:Insertion {{pos: $insert_pos, base: $base}})
+                WITH n
+                MATCH (l:Insertion {{pos: $prev_pos}})
+                MERGE (l)-[:`{read_name}`]->(n)
+                """.strip(),
+                    {
+                        "insert_pos": insert_pos,
+                        "prev_pos": prev_pos,
+                        "base": base,
                     },
                 )
             )
 
-            j -= 1
-            i += 0.01
+        prev_pos = insert_pos
 
-        # merge final insertion node to the next reference sequence node
+    # Link last insertion to the next reference nucleotide
+    if prev_pos is not None:
         queries.append(
             (
-                (
-                    f"""
-                MATCH (s:Insertion {{pos: $insert_position}})
-                MATCH (e:Nucleotide {{pos: $ref_position}})
-                MERGE (s)-[:`{read_name}`]->(e)
-                """
-                ).strip(),
+                f"""
+            MATCH (s:Insertion {{pos: $insert_pos}})
+            MATCH (e:Nucleotide {{pos: $next_ref_pos}})
+            MERGE (s)-[:`{read_name}`]->(e)
+            """.strip(),
                 {
-                    "insert_position": current_position + (i - 0.01),
-                    "ref_position": current_position + 1,
-                },
-            )
-        )
-
-    else:
-        # when there is only 1 insertion node created, its merged with the next reference sequence node here
-        queries.append(
-            (
-                (
-                    f"""
-                MATCH (s:Insertion {{pos: $insert_position}})
-                MATCH (e:Nucleotide {{pos: $ref_position}})
-                MERGE (s)-[:`{read_name}`]->(e)
-                """
-                ).strip(),
-                {
-                    "insert_position": current_position + i,
-                    "ref_position": current_position + 1,
+                    "insert_pos": prev_pos,
+                    "next_ref_pos": start_pos + 1,
                 },
             )
         )
--- alignment_to_graph/app/methods/add_softclip.py
+++ alignment_to_graph/app/methods/add_softclip.py
@@ -1,88 +1,74 @@
 def add_softclip(read, current_position, cigar_tuple):
     queries = []
     read_name = read.query_name
-    start = current_position
-    sequence = read.query_sequence
+    start_pos = current_position
+    softclip_length = cigar_tuple[1]
 
-    # add first softclip base and link it to last reference base
-    queries.append(
-        (
-            (
-                f"""
+    # Get the sequence of inserted bases from the read
+    read_offset = current_position - read.reference_start
+    softclip_bases = read.query_sequence[read_offset : read_offset + softclip_length]
+
+    prev_pos = None
+    for i, base in enumerate(softclip_bases):
+        insert_pos = start_pos + (i + 1)
+
+        if i == 0:
+            # Link first Softclip to reference position
+            queries.append(
+                (
+                    (
+                        f"""
             CREATE (n:Softclip {{pos: $pos, base: $base}})
             WITH n
             MATCH (n:Softclip {{pos: $pos}})
             MATCH (l:Nucleotide {{pos: $last_pos}})
             MERGE (l)-[:`{read_name}`]->(n)
             """
-            ).strip(),
-            {
-                "pos": start,
-                "last_pos": start - 1,
-                "base": sequence[0],
-                "read_name": read_name,
-            },
-        )
-    )
-
-    if cigar_tuple[1] > 1:
-        start += 1  # increment start base by 1 as we've already added the first base
-        positions = list(range(start, (start + cigar_tuple[1]) - 1))
-
-        for pos in positions:
+                    ).strip(),
+                    {
+                        "pos": insert_pos,
+                        "last_pos": start_pos,
+                        "base": base,
+                        "read_name": read_name,
+                    },
+                )
+            )
+        else:
+            # Link Softclip to previous Softclip
             queries.append(
                 (
                     (
                         f"""
-                CREATE (n:Softclip {{pos: $pos, base: $base}})
-                WITH n
-                MATCH (n:Softclip {{pos: $pos}})
-                MATCH (l:Softclip {{pos: $last_pos}})
-                MERGE (l)-[:`{read_name}`]->(n)
-                """
+            CREATE (n:Softclip {{pos: $pos, base: $base}})
+            WITH n
+            MATCH (n:Softclip {{pos: $pos}})
+            MATCH (l:Softclip {{pos: $last_pos}})
+            MERGE (l)-[:`{read_name}`]->(n)
+            """
                     ).strip(),
                     {
-                        "pos": pos,
-                        "last_pos": pos - 1,
-                        "base": sequence[pos - current_position],
+                        "pos": insert_pos,
+                        "last_pos": prev_pos,
+                        "base": base,
                         "read_name": read_name,
                     },
                 )
             )
 
-        # final join
+        prev_pos = insert_pos
+
+    # Link last Softclip to the next reference nucleotide
+    if prev_pos is not None:
         queries.append(
             (
-                (
-                    f"""
-            MATCH (n:Softclip {{pos: $pos}})
-            MATCH (l:Nucleotide {{pos: $next_ref_pos}})
-            MERGE (n)-[:`{read_name}`]->(l)
-            """
-                ).strip(),
+                f"""
+            MATCH (s:Softclip {{pos: $insert_pos}})
+            MATCH (e:Nucleotide {{pos: $next_ref_pos}})
+            MERGE (s)-[:`{read_name}`]->(e)
+            """.strip(),
                 {
-                    "pos": positions[-1],
-                    "next_ref_pos": positions[-1] + 1,
-                    "read_name": read_name,
-                },
-            )
-        )
-
-    else:
-
-        queries.append(
-            (
-                (
-                    f"""
-            MATCH (l:Softclip {{pos: $pos}})
-            MATCH (n:Nucleotide {{pos: $next_ref_pos}})
-            MERGE (n)-[:`{read_name}`]->(l)
-            """
-                ).strip(),
-                {
-                    "pos": positions[-1],
-                    "next_ref_pos": positions[-1] + 1,
-                    "read_name": read_name,
+                    "insert_pos": prev_pos,
+                    "next_ref_pos": insert_pos + 1,
                 },
             )
         )
